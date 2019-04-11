db0491088ad4aa30f0d148d0e406e6e413ac48b5
anoj2017 <anojk1996@gmail.com>
2019-04-11 12:25:34 +0530

    add file

---
+++ index.rb
@@ -0,0 +1,57 @@
+# The Hello Class
+class Hello
+   
+   def evenOdd( )
+      	a=10
+		if(a % 2==0)
+			puts("#{a} is even Number ")
+		else
+ 			puts("#{a} is odd")
+ 		end
+   end
+
+   def graterThen
+  		a=10
+  		b=20
+  		c=5
+  		if a>b and a>c
+  			puts("#{a} is greater")
+  		elsif(b>a and b>c)
+  			puts("#{b} is greater")
+  		else
+  			puts("#{c} is greater")
+  		
+  		end	
+   end
+
+
+   def forloop()
+   		/for i in 0..10
+   			puts(i)
+   		end
+/
+   		/i=0 and while(i<20) do
+		puts i+=1
+		end/
+		for j in 1..5 do
+     	for i in 1..5 do
+         print i,  " "
+         break if i == 2
+    end
+	end
+   	
+   	end		
+
+   def add1( )
+    @b = 20
+    puts(@b)
+   end
+ 
+end
+
+# Create a new object
+h = Hello.new()
+# Output "Hello Ruby!"
+h.evenOdd
+h.graterThen
+h.forloop
